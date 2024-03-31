5a6454a80268801ae2f8c81774187222d55fe935
shreyj786 <jainshrey786@gmail.com>
2024-03-31 11:35:58 +0530

    first game commit

---
+++ Assets/Collision.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+ void OnCollisionEnter2D(Collision2D other) {
+    
+}
+
+ void OnTriggerEnter2D(Collider2D other) {
+    
+}
+}
